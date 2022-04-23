24caa36b073512a5cf3097fff15fe75a7128c97e
luc <lcmusic1994@126.com>
2022-04-23 16:18:54 +0800

    init

---
+++ src/main/java/question100/Question10.java
@@ -0,0 +1,17 @@
+package question100;
+
+/**
+ * @author Lu Chao
+ * @create 2022/4/23
+ */
+public class Question10 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static void question1() {
+
+    }
+}
