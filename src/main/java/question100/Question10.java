5a9bbfdfd2554b2a1f2e5fa3937dd2a360844825
luc <luc@shinemo.com>
2022-05-12 23:22:22 +0800

    question8

--- src/main/java/question100/Question10.java
+++ src/main/java/question100/Question10.java
@@ -21,8 +21,8 @@ public class Question10 {
 //        System.out.println(q.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
 //        System.out.println(q.longestPalindrome("0123456789"));
 //        System.out.println(q.convert("0123456789", 3));
-        System.out.println(q.reverse(901000));
-
+//        System.out.println(q.reverse(901000));
+        System.out.println(q.myAtoi("4193 with words"));
 
     }
 
@@ -452,5 +452,69 @@ public class Question10 {
         }
         return max.length() > newNum.length() || max.compareTo(newNum) > 0 ? Integer.valueOf(newNum) : 0;
 
+    }
+
+    /**
+     * 请你来实现一个 myAtoi(string s) 函数，使其能将字符串转换成一个 32 位有符号整数（类似 C/C++ 中的 atoi 函数）。
+     *
+     * 函数 myAtoi(string s) 的算法如下：
+     *
+     * 读入字符串并丢弃无用的前导空格
+     * 检查下一个字符（假设还未到字符末尾）为正还是负号，读取该字符（如果有）。 确定最终结果是负数还是正数。 如果两者都不存在，则假定结果为正。
+     * 读入下一个字符，直到到达下一个非数字字符或到达输入的结尾。字符串的其余部分将被忽略。
+     * 将前面步骤读入的这些数字转换为整数（即，"123" -> 123， "0032" -> 32）。如果没有读入数字，则整数为 0 。必要时更改符号（从步骤 2 开始）。
+     * 如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
+     * 返回整数作为最终结果。
+     * 注意：
+     *
+     * 本题中的空白字符只包括空格字符 ' ' 。
+     * 除前导空格或数字后的其余字符串外，请勿忽略 任何其他字符。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode.cn/problems/string-to-integer-atoi
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param s
+     * @return
+     */
+    public int myAtoi(String s) {
+        // 直接面向用列编程 ，我直接吐了呀
+        // 还是参照题解吧
+        // 以下出自  https://leetcode.cn/u/sweetiee/
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        int idx = 0;
+        while (idx < n && chars[idx] == ' ') {
+            // 去掉前导空格
+            idx++;
+        }
+        if (idx == n) {
+            //去掉前导空格以后到了末尾了
+            return 0;
+        }
+        boolean negative = false;
+        if (chars[idx] == '-') {
+            //遇到负号
+            negative = true;
+            idx++;
+        } else if (chars[idx] == '+') {
+            // 遇到正号
+            idx++;
+        } else if (!Character.isDigit(chars[idx])) {
+            // 其他符号
+            return 0;
+        }
+        int ans = 0;
+        while (idx < n && Character.isDigit(chars[idx])) {
+            int digit = chars[idx] - '0';
+            if (ans > (Integer.MAX_VALUE - digit) / 10) {
+                // 本来应该是 ans * 10 + digit > Integer.MAX_VALUE
+                // 但是 *10 和 + digit 都有可能越界，所有都移动到右边去就可以了。
+                return negative? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            ans = ans * 10 + digit;
+            idx++;
+        }
+        return negative? -ans : ans;
     }
 }
